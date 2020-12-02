eadc7749be1541161f2da572bb837c17df3af027
shimoda-yuki0419 <73351630+shimoda-yuki0419@users.noreply.github.com>
2020-12-03 00:44:32 +0900

    Add files via upload

---
+++ mynavi_sample - 4.py
@@ -0,0 +1,91 @@
+import os
+from selenium.webdriver import Chrome, ChromeOptions
+import time
+import pandas as pd
+import csv
+
+
+### Chromeを起動する関数
+def set_driver(driver_path,headless_flg):
+    # Chromeドライバーの読み込み
+    options = ChromeOptions()
+
+    # ヘッドレスモード（画面非表示モード）をの設定
+    if headless_flg==True:
+        options.add_argument('--headless')
+
+    # 起動オプションの設定
+    options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36')
+    #options.add_argument('log-level=3')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+    options.add_argument('--incognito')          # シークレットモードの設定を付与
+
+    # ChromeのWebDriverオブジェクトを作成する。
+    return Chrome(executable_path=os.getcwd() + "\\" + driver_path,options=options)
+
+### main処理
+def main():
+    search_keyword=input("")
+    # driverを起動
+    driver=set_driver("chromedriver.exe",False)
+    # Webサイトを開く
+    driver.get("https://tenshoku.mynavi.jp/")
+    time.sleep(5)
+    # ポップアップを閉じる
+    driver.execute_script('document.querySelector(".karte-close").click()')
+    time.sleep(2)
+    # ポップアップを閉じる
+    # driver.execute_script('document.querySelector(".karte-close").click()')
+    
+    # 検索窓に入力
+    driver.find_element_by_class_name("topSearch__text").send_keys(search_keyword)
+    # 検索ボタンクリック
+    driver.find_element_by_class_name("topSearch__button").click()
+    
+
+    # 1ページ分情報取得
+    
+        
+        
+    for i in range(3):  
+            elem_names = driver.find_elements_by_class_name("cassetteRecruit__name")
+            elem_characteristics = driver.find_elements_by_class_name("cassetteRecruit__copy")
+            elem_contents = driver.find_elements_by_class_name("cassetteRecruit__mainM")
+            for elem_name,elem_characteristic,elem_content in zip(elem_names,elem_characteristics,elem_contents):
+                name_list = [elem_name.text,elem_characteristic.text,elem_content.text]
+                for elem_name_one,elem_characteristic_one,elem_content_one in zip(name_list[0],name_list[1],name_list[2]):
+                    elem_name_one = driver.find_element_by_class_name("cassetteRecruit__name")
+                    elem_characteristic_one = driver.find_element_by_class_name("cassetteRecruit__copy")
+                    elem_content_one = driver.find_element_by_class_name("cassetteRecruit__mainM")
+
+                # 終わるまで処理
+                    # next_page=driver.find_elements_by_class_name("iconFont--arrowLeft")
+                    # if len(next_page)>=1:
+                    #     next_page_link=next_page[0].get_attribute("href")
+                    #     driver.get(next_page_link)
+                    # else:
+                    #     print("最終ページです。終了します。")
+                    #     break
+            
+
+                    df = pd.DataFrame({
+                            '会社名':[elem_name_one],
+                            '特徴':[elem_characteristic_one],
+                            '内容':[elem_content_one]}
+                            )
+        
+    
+                    with open('new.csv','a',newline="") as f:
+                            writer = csv.writer(f)
+                            w = writer.writerows(df)
+                            print(w)
+    
+        
+
+
+    
+
+### 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
+if __name__ == "__main__":
+    main()
